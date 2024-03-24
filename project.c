dad3c5e5c53b8c8ede63f6e412b3bbec118cadf7
Inês Martins <ines.o.martins@tecnico.ulisboa.pt>
2024-03-24 12:08:14 +0000

    função de leitura de input

--- project.c
+++ project.c
@@ -1,1 +1,67 @@
 /* iaed24 - ist1107083 - project */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* Definição de limites para comprimento do input e de cada componente do mesmo */
+#define MAXCOMANDO 200
+#define MAXPALAVRAS 20
+
+/* função para interpretação de comando */
+
+char **lerComando (){
+
+    const char separadores[10] = " \t";
+  
+    char comando[MAXCOMANDO];
+    char *token;
+    char *restoComando;
+    int iToken =0;   
+
+    /*Aloca espaço e inicializa*/
+    char **palavrasComando;
+    palavrasComando = malloc(MAXPALAVRAS * sizeof(char*));
+    for (int i = 0; i < MAXPALAVRAS; i++) palavrasComando[i] = NULL;
+        
+    /* Reebe e lê comando */
+    fgets(comando, sizeof(comando), stdin);
+    printf("Comando: %s\n", comando);
+
+    /* Remocao de carater \n e \0 */
+    if (comando[strlen(comando) - 1] == '\n')
+        comando[strlen(comando) - 1] = '\0';
+
+    restoComando = comando;
+
+    while ((token = strtok_r(restoComando, separadores, &restoComando))){
+        palavrasComando[iToken] = malloc((int)strlen(token) + 1); 
+        strcpy(palavrasComando[iToken],token);
+        printf( "Token[%i] - %s\n", iToken, palavrasComando[iToken] );
+        iToken++;
+    }
+
+    return palavrasComando;
+
+}
+
+int main(){
+
+    char **entrada;
+    char operacao;
+
+    do {
+        entrada = lerComando();
+        
+        int i=0;
+        while (entrada[i] != NULL){
+        printf( "Token[%i] - %s\n", i, entrada[i++] );
+        }
+
+        operacao = entrada[0][0];
+
+    }
+    while (operacao != 'q');
+
+    return 0;
+}
